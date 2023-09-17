0783ed193bc935523da1a5732d5a47f2ba72bedf
Manjunath Rm <99001629+prime00125@users.noreply.github.com>
2023-09-17 16:58:01 +0530

    Update Navbar.js

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -2,6 +2,8 @@ import React  from 'react'
 import '../ComStyles/Navbar.css';
 import List from './List';
 import { useState } from 'react';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 
 
@@ -11,6 +13,9 @@ function Navbar() {
   return (
     <div>
       <nav>
+    <section>
+    <FontAwesomeIcon icon={faBars} />
+    </section>
         <section>
       <div className='logo' onClick={()=>Setisopen(true)}></div>
         <span onClick={()=>Setisopen(true)} className='prof'>Profile</span>
